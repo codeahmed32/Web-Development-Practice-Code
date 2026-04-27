bc3e7c5e3b558c5860e7aa7356441cd3473e9c7f
Ahmed Abdullah <abdullahahmad6782@gmail.com>
2026-04-27 11:38:03 +0500

    changes applied - 1

--- Assignments/tinyURL Clone (ReactJS)/src/App.jsx
+++ Assignments/tinyURL Clone (ReactJS)/src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './index.css';
 import Navbar from './components/Navbar';
-import HeroSection from './components/Herosection';
 import FeaturesSection from './components/Features';
 import QuickAndEasySection from './components/QuickSection';
 import BrandingSection from './components/BrandingSection';
 import StatsSection from './components/Statistics';
 import AboveFooter from './components/AboveFooter';
 import Footer from './components/FooterSection';
+import HeroSection from './components/HeroSection';
 
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <HeroSection/>
-      <FeaturesSection/>
-      <QuickAndEasySection/>
-      <BrandingSection/>
-      <StatsSection/>
-      <AboveFooter/>
-      <Footer/>
+      <Navbar />
+      <HeroSection />
+      <FeaturesSection />
+      <QuickAndEasySection />
+      <BrandingSection />
+      <StatsSection />
+      <AboveFooter />
+      <Footer />
     </div>
   )
 }
